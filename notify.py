c2d57a1a63662929a9d1effae8b6d698384b9546
Nathan V-C <nvcarski@gmail.com>
2014-08-29 13:03:37 -0400

    Initial commit

---
+++ notify.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import urllib
+import urllib2
+import smtplib
+import sys
+import httplib
+
+from email.mime.text import MIMEText
+
+from twilio.rest import TwilioRestClient
+
+from config import YO_API_KEY, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_NUMBER, EMAIL_LOGIN_USER, EMAIL_LOGIN_PASSWORD, EMAIL_SMTP_SERVER, EMAIL_SMTP_PORT
+
+
+YO_URL = 'https://api.justyo.co/yo/'
+
+EMAIL_FROM_ADDRESS = 'ncvc@mit.edu'
+
+
+def notifyYo(username):
+	args = { 'api_token': YO_API_KEY, 'username': username }
+	encodedArgs = urllib.urlencode(args)
+
+	try:
+		responseCode = urllib2.urlopen(YO_URL, encodedArgs).getcode()
+	except urllib2.HTTPError:
+		# Probably rate-limited, Yo limits you to 1 Yo per minute
+		return False
+
+	return responseCode == httplib.OK
+
+def notifyText(number, messageStr):
+	client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+	client.messages.create(to=number, from_=TWILIO_NUMBER, body=messageStr)
+
+def notifyEmail(emailAddress, subject, messageStr):
+	msg = MIMEText(messageStr)
+
+	msg['Subject'] = subject
+	msg['From'] = EMAIL_FROM_ADDRESS
+	msg['To'] = emailAddress
+
+	s = smtplib.SMTP_SSL(EMAIL_SMTP_SERVER, EMAIL_SMTP_PORT)
+	s.login(EMAIL_LOGIN_USER, EMAIL_LOGIN_PASSWORD)
+	s.sendmail(EMAIL_FROM_ADDRESS, [emailAddress], msg.as_string())
+	s.quit()
+
+def fromPipe():
+	lines = []
+	for line in sys.stdin:
+		sys.stdout.write(line)
+		lines.append(line)
+
+	return notifyYo('madcow')
+	# notifyText('4108070375', 'Done!')
+	# notifyEmail('nvcarski@gmail.com', 'subj', ''.join(lines))
+
+
+if __name__ == '__main__':
+	fromPipe()
